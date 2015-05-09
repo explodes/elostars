9d69e339d57be598402fe524d6c94aeccc73e59e
Evan Leis <evan.explodes@gmail.com>
2015-05-09 00:33:55 -0600

    [logout] login/logout

--- elostars/main/templatetags/user_stuff.py
+++ elostars/main/templatetags/user_stuff.py
@@ -7,7 +7,7 @@ register = Library()
 @register.inclusion_tag("auth/login_form.html", takes_context=True)
 def login_form(context):
     request = context.request
-    login_form = AuthenticationForm(request.POST or None)
     return {
-        "form": login_form,
+        "request": request,
+        "form": AuthenticationForm(request.POST or None),
     }
